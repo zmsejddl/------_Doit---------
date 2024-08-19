d9a897621204ba05de90bec284be33cf578e4b02
Jung Young Hun <hujung0268@gamil.com>
2024-08-19 11:28:39 +0900

    사용자가 입력한 숫자 가져오기

--- upAndDown/js/upanddown.js
+++ upAndDown/js/upanddown.js
@@ -2,7 +2,7 @@ var count = 0;
 var randomNumber = Math.floor(Math.random() * 100) + 1;
 
 function finding() {
-  var userNumber = document.querySelector("#try").ariaValueMax;
+  var userNumber = document.querySelector("#try").value;
 
   if (userNumber >= 1 && userNumber <= 100) {
     console.log(userNumber);
