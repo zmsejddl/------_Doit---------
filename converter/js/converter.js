cf133683f4408b7a502b56ea70fc67ace6ecc93c
Jung Young Hun <hujung0268@gamil.com>
2024-08-21 12:41:35 +0900

    상태에 따라 단위 바꾸기

---
+++ converter/js/converter.js
@@ -0,0 +1,23 @@
+var sUnit = document.querySelector("#s-unit"); // 왼쪽 필드의 단위.
+var tUnit = document.querySelector("#t-unit"); // 오른쪽 필드의 단위.
+var cTof = true; // 섭씨에서 화씨로 변환 (Celsius To Fahrenheit)
+
+var source = document.querySelector("#s-value"); // 왼쪽 텍스트 필드
+var target = document.querySelector("#t-value"); // 오른쪽 텍스트 필드
+
+function exUnit() {
+  source.value = ""; // 왼쪽 텍스트 필드 값 지움
+  target.value = ""; // 오른쪽 텍스트 필드 값 지움
+
+  if (cTof) {
+    // 현재 '섭씨'에서 '화씨'로 변환 상태라면
+    cTof = false; // '화씨'에서 '섭씨' 변환 상태로 바꿈
+    sUnit.innerHTML = "&#8457;"; // 왼쪽 단위 필드에 화씨 기호
+    tUnit.innerHTML = "&#8451;"; // 오른쪽 단위 필드에 섭씨 기호
+  } else {
+    // 현재 '화씨'에서 '섭씨' 변환 상태라면
+    cTof = true; // '섭씨'에서 '화씨' 변환 상태로 바꿈
+    sUnit.innerHTML = "&#8451;"; // 왼쪽 단위 필드에 섭씨 기호
+    tUnit.innerHTML = "&#8457;"; // 오른쪽 단위 필드에 화씨 기호
+  }
+}
