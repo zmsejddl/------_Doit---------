f6ebc45f3bd8fe1571a31838a9b06733edb922ca
Jung Young Hun <hujung0268@gamil.com>
2024-08-24 14:36:03 +0900

    가져온 시간 값을 1초씩 줄이기

--- timer/js/timer.js
+++ timer/js/timer.js
@@ -12,3 +12,24 @@ function startTimer() {
   }
   timer = setInterval(countTimer, 1000); // 1초(1000밀리초)마다 countTimer() 함수 실행
 }
+
+function countTimer() {
+  if (sec != 0) {
+    // '초' 값이 0이 아닐 때 실행할 명령들
+    sec--;
+    document.querySelector("#display").innerHTML =
+      min + "분 " + sec + "초 남았습니다.";
+  } else {
+    if (min != 0) {
+      // (sec=0 이면서) '분' 값이 0이 아닐 때 실행할 명령
+      min--; // 1분 줄이고
+      sec = 59; // '초' 값을 59초부터 시작
+    } else {
+      // (sec=0 이면서) '분' 값이 0일 때, 즉 타이머가 끝났을 때 실행할 명령
+      clearInterval(timer);
+      document.querySelector("#display").innerText = "타이머 종료";
+      document.querySelector("#startMin").value = "";
+      document.querySelector("#startSec").value = "";
+    }
+  }
+}
